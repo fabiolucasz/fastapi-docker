f47aa3b13e728ec93d0ce6aaf2a05ffe6797d456
Fabio Lucas <fabiolucamz@gmail.com>
2026-03-22 10:37:30 -0300

    fix: resolvido os paths do .env e variaveis de ambiente do docker-compose.yml

--- src/core/config.py
+++ src/core/config.py
@@ -5,7 +5,7 @@ from typing import Literal
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", 
+        env_file="../.env", 
         env_file_encoding="utf-8", 
         env_ignore_empty=True,
         extra="ignore"
